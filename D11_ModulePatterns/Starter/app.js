d3fabccd2d1352d1475d9eb42d9f29476bdb645d
dams <damien.messner@gmail.com>
2017-07-27 13:34:45 +0200

    fin de course 4/26
    Signed-off-by: dams <damien.messner@gmail.com>

--- D11_ModulePatterns/Starter/app.js
+++ D11_ModulePatterns/Starter/app.js
@@ -1,7 +1,6 @@
 var greet = require('./greet1');
-
 greet();
-
+//on importe directement la propriété que l'on souhaite
 var greet2 = require('./greet2').greet;
 greet2();
 
@@ -9,10 +8,14 @@ var greet3 = require('./greet3');
 greet3.greet();
 greet3.greeting = 'Changed Hello world!';
 
+//require sur un fichier déjà importé ne change rien
+//c'est déjà en cache
 var greet3b = require('./greet3');
 greet3b.greet();
 
 var Greet4 = require('./greet4');
 objGreet4 = new Greet4();
-
 objGreet4.greet();
+
+var greet5 = require('./greet5');
+greet5.greet();
---
+++ D11_ModulePatterns/Starter/greet5.js
@@ -0,0 +1,11 @@
+var greeting = 'Hello world!!!!';
+
+function greet()
+{
+    console.log(greeting);
+}
+
+module.exports = 
+{
+    greet:greet
+};
