ece71c90819b2b24b9b609b9b6680cc408a43b5a
dams <damien.messner@gmail.com>
2017-07-27 13:33:10 +0200

    fin de courses 4/26

---
+++ D11_ModulePatterns/Starter/app.js
@@ -0,0 +1,18 @@
+var greet = require('./greet1');
+
+greet();
+
+var greet2 = require('./greet2').greet;
+greet2();
+
+var greet3 = require('./greet3');
+greet3.greet();
+greet3.greeting = 'Changed Hello world!';
+
+var greet3b = require('./greet3');
+greet3b.greet();
+
+var Greet4 = require('./greet4');
+objGreet4 = new Greet4();
+
+objGreet4.greet();
