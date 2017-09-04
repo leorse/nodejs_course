357d486eb8e12933cb3afffbc34defc0828c5c61
dams <damien.messner@gmail.com>
2017-09-04 14:06:18 +0200

    fin section 6/47

---
+++ F6_Buffers/Starter/app.js
@@ -0,0 +1,10 @@
+var buf = new Buffer('Salut', 'utf8');
+console.log(buf);
+console.log(buf.toString());
+console.log(buf.toJSON());
+console.log(buf[2]);
+buf.write('wo');
+console.log(buf.toString());
+console.log(buf.toString());
+console.log(buf.toString());
+console.log(`Donc: ${buf.toString()}`);
