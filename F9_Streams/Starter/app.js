3045723b04064ff2f339610481860bdf66d8448f
dams <damien.messner@gmail.com>
2017-09-07 13:38:11 +0200

    fin 6/51

---
+++ F9_Streams/Starter/app.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 32 * 1024 });
+var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+
+readable.on('data', function(chunk) {
+	console.log(chunk.length);
+	writable.write(chunk);
+});
